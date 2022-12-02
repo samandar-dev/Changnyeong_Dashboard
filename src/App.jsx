5079f0c8d4baac5bcb24c1a48c7f16c20ce7219f
Samandar Bohodirov <samandarbohodirov@Samandars-Mac-mini.local>
2022-12-02 18:54:35 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Title from './companents/Title/Title';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Title />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/companents/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Title.scss";
+
+export default function Title() {
+    return (
+        <>
+            <div className="title-box">
+                <h1>실시간 유동인구 현황</h1>
+                <p>자연의 안녕, 창녕군</p>
+            </div>
+        </>
+    )
+}
