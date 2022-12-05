b9fcddc57ec5ccdb15d2b2cd0e3b654ef58000cf
Samandar Bohodirov <samandarbohodirov@Samandars-Mac-mini.local>
2022-12-05 13:43:09 +0500

    weel top spots section start

---
+++ src/companents/WeeklyTopSpots/WeeklyTopSpots.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./WeeklyTopSpots.scss";
+
+export default function WeeklyTopSpots() {
+    return (
+        <>
+            <section className='weekSpots'>
+
+            </section>
+        </>
+    )
+}
